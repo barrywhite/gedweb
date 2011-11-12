cae2f09397a26fa14332eebce1732d4a1375c771
barry white <bw_souza@hotmail.com>
2011-11-12 00:22:30 -0300

    teste comite

--- src/main/java/br/com/modeloArquitetura/controlador/OlaMundo.java
+++ src/main/java/br/com/modeloArquitetura/controlador/OlaMundo.java
@@ -24,4 +24,27 @@ public class OlaMundo implements Serializable {
 	public void setTeste(String teste) {
 		this.teste = teste;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OlaMundo other = (OlaMundo) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
