ec0218b2cef66b2c7068169edfa6fafe7d74f49b
barry white <bw_souza@hotmail.com>
2011-11-12 00:17:07 -0300

    teste

--- src/main/java/br/com/modeloArquitetura/controlador/OlaMundo.java
+++ src/main/java/br/com/modeloArquitetura/controlador/OlaMundo.java
@@ -6,17 +6,22 @@ import java.io.Serializable;
 @ManagedBean
 @SessionScoped
 public class OlaMundo implements Serializable {
- 
-	
 	
 	private static final long serialVersionUID = 1L;
  
 	private String name;
+	private String teste;
  
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getTeste() {
+		return teste;
+	}
+	public void setTeste(String teste) {
+		this.teste = teste;
 	}
 }
