301fb8975cf86b15fda463d86851dc34c3900b26
wagner farias <fariaswag@gmail.com>
2011-11-26 16:43:29 -0300

    inciando modal
    Change-Id: Iab3021633ce51945e27e97af194df3a89f38c49c

---
+++ src/main/java/br/com/gedweb/controlador/modal/ControladorModalPessoa.java
@@ -0,0 +1,52 @@
+package br.com.gedweb.controlador.modal;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.gedweb.crud.CrudGenerico;
+import br.com.gedweb.modelo.Pessoa;
+
+@ManagedBean(name = "controladorModalPessoa")
+@SessionScoped
+public class ControladorModalPessoa {
+
+	private List<Pessoa> pessoas;
+	private Pessoa pessoaSelecionada;
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("unidadePersistence");
+	EntityManager em = emf.createEntityManager();
+
+	public ControladorModalPessoa() {
+	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void iniciar() {
+		@SuppressWarnings("rawtypes")
+		CrudGenerico crud =  new CrudGenerico(Pessoa.class, this.em);
+		this.pessoas = crud.buscarTodos();
+	}	
+	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		iniciar();
+		this.pessoas = pessoas;
+	}
+
+	public Pessoa getPessoaSelecionada() {
+		return pessoaSelecionada;
+	}
+
+	public void setPessoaSelecionada(Pessoa pessoaSelecionada) {
+		this.pessoaSelecionada = pessoaSelecionada;
+	}
+
+}
